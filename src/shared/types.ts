8e6c5c648b11ab3b33b959a94f95ca3083d39a1c
Liana Baratian <lianabaratian7@gmail.com>
2026-06-19 12:28:26 +0300

    Add shared types and IPC channel constants

---
+++ src/shared/types.ts
@@ -0,0 +1,39 @@
+export interface OsInfo {
+  platform: string       
+  distro: string       
+  release: string   
+  arch: string        
+  hostname: string
+  username: string | null 
+}
+
+export interface ProcessInfo {
+  pid: number
+  name: string
+  cpu: number    
+  memory: number 
+}
+
+export interface MemoryInfo {
+  total: number      
+  used: number     
+  free: number       
+  usedPercent: number 
+}
+
+export interface DiskInfo {
+  mount: string
+  total: number       
+  used: number        
+  free: number        
+  usedPercent: number 
+}
+
+export const IPC_CHANNELS = {
+  GET_OS_INFO: 'system:get-os-info',
+  GET_PROCESSES: 'system:get-processes',
+  GET_MEMORY_INFO: 'system:get-memory-info',
+  GET_DISK_INFO: 'system:get-disk-info',
+} as const
+
+export type IpcChannel = (typeof IPC_CHANNELS)[keyof typeof IPC_CHANNELS]
