ad365959885002a79c4fa60c4f9c3c3d052a6251
Liana Baratian <lianabaratian7@gmail.com>
2026-06-19 19:21:28 +0300

    Remove generic ipcRenderer bridge from preload for a minimal IPC surface

--- electron/preload.ts
+++ electron/preload.ts
@@ -2,26 +2,6 @@ import { ipcRenderer, contextBridge } from 'electron'
 import { IPC_CHANNELS } from '../src/shared/types'
 import type { SystemApi } from '../src/shared/types'
 
-// --------- Expose the generic ipcRenderer bridge (from template) ---------
-contextBridge.exposeInMainWorld('ipcRenderer', {
-  on(...args: Parameters<typeof ipcRenderer.on>) {
-    const [channel, listener] = args
-    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
-  },
-  off(...args: Parameters<typeof ipcRenderer.off>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.off(channel, ...omit)
-  },
-  send(...args: Parameters<typeof ipcRenderer.send>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.send(channel, ...omit)
-  },
-  invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.invoke(channel, ...omit)
-  },
-})
-
 const systemApi: SystemApi = {
   getOsInfo: () => ipcRenderer.invoke(IPC_CHANNELS.GET_OS_INFO),
   getProcesses: () => ipcRenderer.invoke(IPC_CHANNELS.GET_PROCESSES),
